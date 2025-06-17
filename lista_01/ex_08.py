f0cc32b0c271549eb2ce59910d3ea72e9570fa3f
Luizlost01 <luized_sparvoli@hotmail.com>
2025-06-17 09:38:49 -0300

    ✅ Resolução Exercicio 08

---
+++ lista_01/ex_08.py
@@ -0,0 +1,8 @@
+#Entrada de dados
+valor_a = int(input('Insira um valor para a variavel a: '))
+valor_b = int(input('Insira um valor para a variavel b: '))
+
+valor_a, valor_b = valor_b, valor_a
+
+#Saida de dados
+print(f'Valor de A: {valor_a}, Valor de B: {valor_b}')
