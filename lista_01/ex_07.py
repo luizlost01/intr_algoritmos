b88d02662c05583633185531182e9df74abed4f4
Luizlost01 <luized_sparvoli@hotmail.com>
2025-06-17 09:28:29 -0300

    ✅ Resolução Exercicio 07

--- lista_01/ex_07.py
+++ lista_01/ex_07.py
@@ -1,13 +1,16 @@
-"""Efetue o cálculo da quantidade de litros de combustível gasta em
-uma viagem, utilizando um automóvel que faz 12 km por litro. Para
-obter o cálculo, o usuário deve fornecer o tempo gasto e a
-velocidade média durante a viagem. Desta forma, será possível
-obter a distância percorrida com a fórmula DISTANCIA = TEMPO *
-VELOCIDADE. Tendo o valor da distância, basta calcular a
-quantidade de litros de combustível utilizada na viagem com a
-fórmula: LITROS USADOS = DISTANCIA / 12. O algoritmo deve apresentar os valores da velocidade média, tempo gasto na
-viagem, a distância percorrida e a quantidade de litros utilizada na
-viagem."""
+#Entrada de Dados
+tempo_viagem = float(input("Informe o tempo gasto na viagem (em horas): "))
+velocidade_media = float(input("Informe a velocidade média (em km/h): "))
 
+km_litro = 12
+distancia = tempo_viagem * velocidade_media
+litros_usados = distancia / km_litro
 
+#Saida de dados
+print(
+    f"Sua velocidade média foi de {velocidade_media} km/h\n"
+    f"O tempo gasto em sua viagem foi de {tempo_viagem} horas\n"
+    f"Sua distância percorrida foi {distancia} km\n"
+    f"Quantidade de litros utilizada na viagem: {litros_usados:.2f} litros"
+)
 
