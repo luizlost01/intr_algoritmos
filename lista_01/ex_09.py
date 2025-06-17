29c543ea7aef0d88f6c906236abd7f865fdd4b0b
Luizlost01 <luized_sparvoli@hotmail.com>
2025-06-17 09:40:34 -0300

    ✅ Resolução Exercicio 09

---
+++ lista_01/ex_09.py
@@ -0,0 +1,13 @@
+'''9. Calcule e apresente o volume de uma caixa retangular, utilizando
+a fórmula: VOLUME = COMPRIMENTO * LARGURA * ALTURA'''
+
+
+#Entrada de dados
+comprimento = float(input('Insira o comprimento da caixa: '))
+largura = float(input('Insira a largura da caixa: '))
+altura = float(input('Insira a altura da caixa: '))
+
+volume = comprimento * largura * altura
+
+#Saida de dados
+print(f'O volume da caixa retangular é: {volume}')
