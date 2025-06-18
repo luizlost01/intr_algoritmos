4a5015656e6756a70e898b15ebec7f0323c6e752
Luizlost01 <luized_sparvoli@hotmail.com>
2025-06-17 22:23:15 -0300

    ✅ Resolução Exercicio 10

---
+++ lista_01/ex_10.py
@@ -0,0 +1,8 @@
+#Entrada de dados
+valor_a = int(input('Insira um valor para a variavel a: '))
+valor_b = int(input('Insira um valor para a variavel b: '))
+
+difrencaQuadrados = (valor_a - valor_b) * (valor_a - valor_b)
+
+#Saida de dados
+print(f'A diferença entre os quadrados de A e B é: {difrencaQuadrados}')
