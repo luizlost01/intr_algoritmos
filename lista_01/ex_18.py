10e8870648b9459d274fb9043e50d49f4297c369
Luizlost01 <luized_sparvoli@hotmail.com>
2025-06-17 22:28:26 -0300

    💡 Remover comentario das instruções

--- lista_01/ex_18.py
+++ lista_01/ex_18.py
@@ -1,21 +1,3 @@
-"""Em uma eleição sindical concorreram ao cargo de presidente
-três candidatos (A, B e C). Durante a apuração dos votos foram
-computados votos nulos e votos em branco, além dos votos válidos
-para cada candidato. Deve ser criado um algoritmo que faça a
-leitura da quantidade de votos válidos para cada candidato, além
-de efetuar também a leitura da quantidade de votos nulos e votos
-em branco. Ao final, o programa deve apresentar o número total de
-eleitores, considerando votos válidos, nulos e em branco; o
-percentual correspondente de votos válidos do candidato A em
-relação à quantidade de eleitores; o percentual correspondente de
-votos válidos do candidato B em relação à quantidade de eleitores;
-o percentual correspondente de votos válidos do candidato C em
-relação à quantidade de eleitores; o percentual correspondente de
-votos nulos em relação à quantidade de eleitores; e por último o
-percentual correspondente de votos em branco em relação à
-quantidade de eleitores."""
-
-
 # Leitura dos votos válidos para cada candidato
 votos_a = int(input("Digite a quantidade de votos válidos para o candidato A: "))
 votos_b = int(input("Digite a quantidade de votos válidos para o candidato B: "))
@@ -38,4 +20,3 @@ print(f"Percentual de votos válidos do candidato B: {percentual_b}")
 print(f"Percentual de votos válidos do candidato C: {percentual_c}")
 print(f"Percentual de votos nulos: {percentual_nulos}")
 print(f"Percentual de votos em branco: {percentual_brancos}")
-# Fim do programa
