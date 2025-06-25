1b67715c12b004c53cf3a7d9136b2709943c2f93
DecksBuss <derickbuss@gmail.com>
2025-06-25 00:20:28 -0300

    Update ex_10.py
    cole fml

--- revisao/ex_10.py
+++ revisao/ex_10.py
@@ -1,7 +1,6 @@
-a = 0 
-c = 0
-
-while c <= 100:
-    print(a)
-    c += 1
-    a = c + a
+c=0
+a=0
+while c<=100:
+    a=a+c
+    c=c+1
+print(f'O resultdo da soma de todos inteiros de 0 a 100 é: {a}')
